fed1323089e2a5b92b3993e327c7ca9382e8cb07
Ray Lv <ray.lv@sap.com>
2015-07-30 18:44:32 +0800

    20150730

--- ElearningDemo/app.js
+++ ElearningDemo/app.js
@@ -4,8 +4,8 @@
  */
 
 var express = require('express')
-  , routes = require('./routes')
-  , user = require('./routes/user')
+  , routes = require('./routes/index')
+  , users = require('./routes/user')
   , http = require('http')
   , path = require('path');
 
@@ -14,7 +14,9 @@ var app = express();
 // all environments
 app.set('port', process.env.PORT || 3000);
 app.set('views', __dirname + '/views');
-app.set('view engine', 'ejs');
+//app.set('view engine', 'ejs');
+app.engine('html', require('ejs').renderFile);
+app.set('view engine', 'html');
 app.use(express.favicon());
 app.use(express.logger('dev'));
 app.use(express.bodyParser());
@@ -22,13 +24,20 @@ app.use(express.methodOverride());
 app.use(app.router);
 app.use(express.static(path.join(__dirname, 'public')));
 
+app.use('/', routes);
+app.use('/user', users);
+app.use('/login', routes.login);
+app.use('/logout', routes);
+app.use('/home', routes);
+
 // development only
 if ('development' == app.get('env')) {
   app.use(express.errorHandler());
 }
 
 app.get('/', routes.index);
-app.get('/users', user.list);
+app.get('/users', users.list);
+app.post('/login', routes.loginpost);
 
 http.createServer(app).listen(app.get('port'), function(){
   console.log('Express server listening on port ' + app.get('port'));
--- ElearningDemo/routes/index.js
+++ ElearningDemo/routes/index.js
@@ -1,8 +1,18 @@
-
-/*
- * GET home page.
- */
-
 exports.index = function(req, res){
   res.render('index', { title: 'Express' });
+};
+
+exports.login = function(req, res){
+  res.render('login', { title: 'Express' });
+};
+
+exports.loginpost = function(req, res){
+	var user={
+	        username: 'admin',
+	        password: '123456'
+	    }
+	    if(req.body.username === user.username && req.body.password === user.password){
+	        res.redirect('/home');
+	    }
+	    res.redirect('/login');
 };
